e58c9636fa39459457df9bed9dec3bc956395c71
Alan Shabrandi <alan.shabrandi@gmail.com>
2026-06-26 23:16:41 +0330

    refactor

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import ChatArea from "@/components/chat/ChatArea";
-import Header from "@/components/layout/Header";
-import Sidebar from "@/components/layout/Sidebar";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -14,17 +11,5 @@ export default function Home() {
     router.replace(`/chat/${newChatId}`);
   }, [router]);
 
-  return (
-    <main className="h-screen bg-background text-foreground">
-      <div className="flex h-full">
-        <Sidebar />
-
-        <section className="flex min-w-0 flex-1 flex-col">
-          <Header />
-
-          <ChatArea />
-        </section>
-      </div>
-    </main>
-  );
+  return <></>;
 }
