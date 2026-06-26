c373ca6ebef7d6e49ca09c0c4f5a40c433cbb975
Alan Shabrandi <alan.shabrandi@gmail.com>
2026-06-26 19:48:40 +0330

    refactor

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(lifespan=lifespan)
 ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://chatgpt-clone-hazel-nine.vercel.app/"
+    "https://chatgpt-clone-hazel-nine.vercel.app"
 ]
 
 FRONTEND_URL = os.getenv("FRONTEND_URL")
